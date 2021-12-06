9f0e057026f153977fea9006ca8a394b1a4024b3
Marina <maribars93@yandex.ru>
2021-12-07 01:01:40 +0300

    Create project & setup linters

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  rules: {
+    "import/prefer-default-export": "off",
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+  },
+};
