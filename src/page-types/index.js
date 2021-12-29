f50d656b24596f29aac6c78f34a22e0385c8f1f4
Marina <maribars93@yandex.ru>
2021-12-29 15:15:25 +0300

    Add adaptive header, footer & mainPage

--- src/page-types/index.js
+++ src/page-types/index.js
@@ -1,1 +1,19 @@
 import "../sass/mainPage-style.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu");
+  const navItem = document.querySelectorAll(".nav_item");
+  const hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+  });
+
+  navItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
+    });
+  });
+});
